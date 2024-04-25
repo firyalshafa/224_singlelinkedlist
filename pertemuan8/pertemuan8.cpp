e48abbb7c11f5dedd0a20cbbc08b1066c015072f
firyalshafa <shafafiryal782@gmail.com>
2024-04-25 15:26:18 +0800

    Add project files.

---
+++ pertemuan8/pertemuan8.cpp
@@ -0,0 +1,16 @@
+// pertemuan8.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+struct node {
+    int noMhs;
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+
